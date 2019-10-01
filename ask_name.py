c23c89283d563b4bfceed309aef734d33a52c1ce
unknown <kshitijyadav147258@gmail.com>
2019-10-01 17:40:45 +0530

    ask name

--- ask_name.py
+++ ask_name.py
@@ -1,5 +1,5 @@
 #this program says hello and asks for my name.
 print("Hello user...")
-name = str(input("What is your name?"))
+name = str(input("What is your name?")) # it get your name and convert into the string
 print("It is good to meet you ", name)
 print("The length of your name ", len(name))
