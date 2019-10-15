9805b976c781a8b0640156185435b456b0798ae1
unknown <kshitijyadav147258@gmail.com>
2019-10-15 17:41:51 +0530

    Factorial number

---
+++ mathCalc/factorial_of_a_number.py
@@ -0,0 +1,18 @@
+import sys
+number = 0
+try:
+	number = int(sys.argv[1])
+except:
+	number = int(input("Enter number: "))
+print("File name: ", sys.argv[0])
+
+# recursion function
+def factorial(num):
+	if (num == 1 or num == 0): # base condition
+		return 1
+	else:
+		return num * factorial(num - 1)
+	
+print("Number: ", number)
+print("Factorial: ", factorial(number))
+	
