884a2444f3bc3b5eb0be6fc3468a9b158014e449
unknown <kshitijyadav147258@gmail.com>
2019-10-15 17:55:50 +0530

    Simple Interest

--- enumInPy.py
+++ enumInPy.py
@@ -1,11 +1,14 @@
 from enum import Enum
 class Color(Enum):
+	"""
+	Enums have been backported from python 3.4. You can get this the enum34 backport from PyPI.
+	pip install enum34
+	 
+	"""
 	red = 1
 	green = 2
 	blue = 3
-	
-	
-	print(Color.red)
+		print(Color.red)
 print(Color(1))
 print(Color(2))
 print(Color['blue'])
--- mathCalc/simpleInterest.py
+++ mathCalc/simpleInterest.py
@@ -1,1 +1,20 @@
 #! python3
+import sys
+# Simple interest  = (Principal * rate * time) / 100
+principal, rate, time = 0, 0, 0
+try:
+	principal = float(sys.argv[1])
+	rate = float(sys.argv[2])
+	time = float(sys.argv[3])
+except:
+	principal = float(input("Enter principal: "))
+	rate = float(input("Enter rate of interest: "))
+	time = float(int(input("Enter time: ")))
+	
+simpleInterest = (principal * rate * time) / 100.0
+
+print("Principal amount: ", principal)
+print("Rate of interest: ", rate)
+print("Time period: ", time)
+print("Total simple interest is ", simpleInterest)
+ 	
